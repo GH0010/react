48d2468580274bdc7be81461be32a433c0ac58ba
DESKTOP-D179MQQ\USER <cocohkd@naver.com>
2024-03-05 17:44:23 +0900

    240305 뷰 연습

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <template>
-<div id="app">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</div>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/todo">Todo</router-link> |
+    <router-link to="/notice">Notice</router-link> |
+    <router-link to="/UserDetails">UserDetails</router-link> |
+
+  </nav>
+  <router-view/>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -22,6 +17,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/MyText.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <input type="text" v-model="text">
+    <button type="button" @click="$emit('text',text)">전달</button>
+  </div>
+</template>
+<script>
+export default {
+  name:'MyText',
+  data(){
+    return{text:""}
+  }
+}
+</script>
+<style>
+  
+</style>
